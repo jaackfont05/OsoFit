b87374f7c7eb92e683699b6484423637363b79f5
jaackfont05 <167032731+jaackfont05@users.noreply.github.com>
2025-10-20 17:02:43 -0500

    added main function (blank), added methods to trainer and weightGoal

--- main/src/main/java/trainer.java
+++ main/src/main/java/trainer.java
@@ -1,3 +1,8 @@
 public class trainer extends user{
-
+    public trainer(String username, String password){
+        super(username,password);
+    }
+    public trainer(){
+        super();
+    }
 }
--- main/src/main/java/weightGoal.java
+++ main/src/main/java/weightGoal.java
@@ -1,3 +1,26 @@
 // Savannah Johnson
 public class weightGoal {
+   private int currWeight;
+   private int goalWeight;
+
+   public weightGoal(int currWeight, int goalWeight) {
+       this.currWeight = currWeight;
+       this.goalWeight = goalWeight;
+   }
+
+   public int getCurrWeight() {
+       return currWeight;
+   }
+
+   public void setCurrWeight(int currWeight) {
+       this.currWeight = currWeight;
+   }
+
+   public int getGoalWeight() {
+       return goalWeight;
+   }
+
+   public void setGoalWeight(int goalWeight) {
+       this.goalWeight = goalWeight;
+   }
 }
